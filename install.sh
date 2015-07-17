1d57af5754bac87f579ab0ad3a7be0ffa7a3e69b
Henri Cavalcante <hnri_mxel@hotmail.com>
2015-07-17 14:42:46 -0300

    added conditional for mac or linux

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
-brew install tmux
-brew install reattach-to-user-namespace
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    brew install tmux
+    brew install reattach-to-user-namespace
+    brew install vim
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt-get install tmux
+    sudo apt-get install vim
+    sudo apt-get install curl
+fi
+
 mkdir NeoBundle
 curl -o NeoBundle/install.sh https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh
 sh ./NeoBundle/install.sh
