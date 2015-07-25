3302317fb0e74cefc2b4e8474339a027897a78f6
Henri Cavalcante <hnri_mxel@hotmail.com>
2015-07-25 11:35:55 -0300

    tmuxpowerline

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
+    brew install https://raw.github.com/Homebrew/homebrew-dupes/master/grep.rb
     brew install tmux
     brew install reattach-to-user-namespace
     brew install vim
@@ -9,6 +10,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo apt-get install vim
     sudo apt-get install curl
 fi
+
+git clone https://github.com/erikw/tmux-powerline.git ~/tmux-powerline
 
 mkdir NeoBundle
 curl -o NeoBundle/install.sh https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh
